073280138f700b42fefccb8b5d9c8c424c0d18d7
Bradyq <braydenquo@gmail.com>
2025-11-14 10:38:06 -0500

    change:

--- build_script.sh
+++ build_script.sh
@@ -12,3 +12,26 @@ wasm-bindgen target/wasm32-unknown-unknown/release/courses_data.wasm \
   --out-dir target/wasm \
   --typescript \
   --target web
+
+cargo install wasm-opt
+wasm-opt target/wasm/*.wasm -o target/wasm/*.wasm-opt.wasm -O
+
+mv target/wasm/*.wasm-opt.wasm target/wasm/*.wasm
+
+cat > target/wasm/package.json <<'EOF'
+{
+  "name": "courses-data",
+  "type": "module",
+  "version": "0.1.0",
+  "files": [
+    "courses_data_bg.wasm",
+    "courses_data.js",
+    "courses_data.d.ts"
+  ],
+  "main": "courses_data.js",
+  "types": "courses_data.d.ts",
+  "sideEffects": [
+    "./snippets/*"
+  ]
+}
+EOF
