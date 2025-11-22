bfa4da0e1c7e1f03e13d316ac4f3a332e83db477
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-11-22 17:28:46 -0500

    Finished writing skeleton code

--- index.ts
+++ index.ts
@@ -51,6 +51,74 @@ worker.onmessage = (event) => {
   console.log(event.data);
 };
 
-export function get_data(): [string, string] {
+/**
+ * WHEN WORKING ON CODEBASE, DO NOT FUCK WITH THE INDEXEDDB 
+ * NAME/CONFIG RANDOMLY. IF YOU DO, MIGHT REQUIRE ALL USERS TO 
+ * REDOWNLOAD THE SEARCH INDEX
+ */
+
+/**
+ * Gets the local version of the search index
+ * @param indexeddb stores the data
+ * @returns version
+ */
+function get_db_version(indexeddb: string): string {
+  return "0.0.1";
+}
+
+/**
+ * Gets the servers version of the search index
+ * @returns version
+ */
+function get_latest_version(): string {
+  return "0.0.1"; // TODO: versioning standard
+}
+
+/**
+ * Actually downloads the data from the sources
+ * @returns [coursesData, searchIndex]
+ */
+function fetch_data_from_server(): [string, string] {
   return ["todo", "todo"];
+}
+
+/**
+ * 
+ * @param indexedb database to fetch data from
+ * @returns [coursesData, searchIndex]
+ */
+function fetch_data_from_db(indexedb: string): [string, string] {
+  return ["todo", "todo"];
+}
+
+/**
+ * 
+ * @param indexedb used to store data
+ * @param data [coursesData, searchIndex]
+ * @param version version found on server
+ */
+function store_data(indexedb: string, data: [string, string], version: string): void {
+
+}
+
+/**
+ * Does all the logic to fetch the local search engine
+ * files
+ * @returns [coursesData, searchIndex] found in 
+ *          search.js
+ */
+export function get_data(): [string, string] {
+  // logic to initialize the DB
+  let indexedb: string = "foo";
+
+  let local_version: string = get_db_version(indexedb);
+  let server_version: string = get_latest_version();
+
+  if (local_version == server_version) {
+    return fetch_data_from_db(indexedb);
+  }
+
+  let data: [string, string] = fetch_data_from_server();
+  store_data(indexedb, data, server_version);
+  return data;
 }
